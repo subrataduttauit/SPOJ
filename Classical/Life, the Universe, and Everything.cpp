2f04ab8e57c673a97ebd75b6bea8edd3e76af160
subrataduttauit <subrataduttauit@gmail.com>
2015-08-28 23:05:34 +0530

    solution

---
+++ Classical/Life, the Universe, and Everything.cpp
@@ -0,0 +1,28 @@
+/* 
+ * Solution to SPOJ Classical Problems
+ * by Subrata Dutta
+ * 
+ * University Institute of Technology, Burdwan University
+ * Dept. of Information Technology
+ * https://github.com/subrataduttauit
+ * https://in.linkedin.com/in/subrataduttauit
+ */
+
+
+#include<iostream>
+#include<cstdio>
+
+using namespace std;
+
+int main(){
+    int n;
+    
+    while(1){
+        scanf("%d",&n);
+        if(n==42) break;
+        
+        printf("%d\n",n);
+    }
+    
+    return 0;
+}
