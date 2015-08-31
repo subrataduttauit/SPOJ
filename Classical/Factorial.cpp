e2e3140c305ca057a6c14615d7aa2ee12b2b5a31
subrataduttauit <subrataduttauit@gmail.com>
2015-08-31 21:36:38 +0530

    solution

---
+++ Classical/Factorial.cpp
@@ -0,0 +1,34 @@
+/* 
+ * Solution to SPOJ Classical Problems
+ * by Subrata Dutta
+ * 
+ * University Institute of Technology, Burdwan University
+ * Dept. of Information Technology
+ * https://github.com/subrataduttauit
+ * https://in.linkedin.com/in/subrataduttauit
+ */
+
+
+#include <cstdio>
+
+using namespace std;
+
+int main(){    
+    int M[12]={5,25,125,625,3125,15625,78125,390625,1953125,9765625,48828125,244140625};
+    
+    int T,N,cont;
+    
+    scanf("%d",&T);
+    
+    for(int tc=1;tc<=T;tc++){
+        scanf("%d",&N);
+        
+        cont=0;
+        
+        for(int j=0;j<12;j++) cont+=N/M[j];
+        
+        printf("%d\n",cont);
+    }
+    
+    return 0;
+}
