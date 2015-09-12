f9f11751c2c392fb4aca9841f213b855acdcfd9f
subrataduttauit <subrataduttauit@gmail.com>
2015-09-12 20:06:48 +0530

    solution

---
+++ Classical/Two Ends.cpp
@@ -0,0 +1,50 @@
+/* 
+ * Solution to SPOJ Classical Problems
+ * by Subrata Dutta
+ * 
+ * University Institute of Technology, Burdwan University
+ * Dept. of Information Technology
+ * https://github.com/subrataduttauit
+ * https://in.linkedin.com/in/subrataduttauit
+ */
+
+
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+int C[1000],memo[1000][1000];
+
+int solve(int a, int b, int p){
+	if(a>b) return 0;
+	
+	int &ret = memo[a][b];
+	if(ret!=-1) return ret;
+	
+	if(p==0) ret = max(C[a]-solve(a+1,b,1-p),C[b]-solve(a,b-1,1-p));
+	else{
+		if(C[a]>=C[b]) ret = C[a]-solve(a+1,b,1-p);
+		else ret = C[b]-solve(a,b-1,1-p);
+	}
+	
+	return ret;
+}
+
+int main(){
+	int tc = 1,N;	
+	
+	while(true){
+		scanf("%d",&N);
+		if(N==0) break;
+		
+		for(int i=0;i<N;++i) scanf("%d",&C[i]);
+		
+		memset(memo,-1,sizeof(memo));
+		printf("In game %d, the greedy strategy might lose by as many as %d points.\n",tc,solve(0,N-1,0));
+		++tc;
+	}
+	
+	return 0;
+}
